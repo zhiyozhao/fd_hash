2497cf15f8e2eeefe2f70b437c89a794a26c868f
xirenhua <xirenhua@outlook.com>
2022-09-27 10:50:10 +0800

    name_only support

--- fd_hash/main.py
+++ fd_hash/main.py
@@ -17,25 +17,29 @@ def read_by_chunk(file_path, chunk_size=4096):
             yield chunk
 
 
-def gen_(root_path):
+def gen_(root_path, name_only):
     if not osp.islink(root_path) and osp.isdir(root_path):
         root_node = {}
         for name in os.listdir(root_path):
-            if name.startswith('.'):
+            if name.startswith("."):
                 continue
             path = osp.join(root_path, name)
-            root_node[name] = gen_(path)
+            root_node[name] = gen_(path, name_only)
 
         return root_node
     elif osp.islink(root_path):
-        content = bytes(os.readlink(root_path), encoding="utf-8")
-        res = getattr(hashlib, ALGO)(content).hexdigest()
+        res = None
+        if not name_only:
+            content = bytes(os.readlink(root_path), encoding="utf-8")
+            res = getattr(hashlib, ALGO)(content).hexdigest()
     elif osp.isfile(root_path):
-        hasher = getattr(hashlib, ALGO)()
-        chunks = read_by_chunk(root_path)
-        for chunk in chunks:
-            hasher.update(chunk)
-        res = hasher.hexdigest()
+        res = None
+        if not name_only:
+            hasher = getattr(hashlib, ALGO)()
+            chunks = read_by_chunk(root_path)
+            for chunk in chunks:
+                hasher.update(chunk)
+            res = hasher.hexdigest()
     else:
         assert (
             False
@@ -44,30 +48,24 @@ def gen_(root_path):
     return res
 
 
-def check_(root_path, org, cur):
+def check_(root_path, org, cur, name_only):
     if not osp.islink(root_path) and osp.isdir(root_path):
-        org_names = set(org.keys())
-        cur_names = set(cur.keys())
+        org_names = set([key for key in org.keys() if not key.startswith(".")])
+        cur_names = set([key for key in cur.keys() if not key.startswith(".")])
         insc = org_names & cur_names
         org_only = org_names - cur_names
         cur_only = cur_names - org_names
         for name in org_only:
-            if name.startswith('.'):
-                continue
             path = osp.join(root_path, name)
             print(f"d: {path}")
         for name in cur_only:
-            if name.startswith('.'):
-                continue
             path = osp.join(root_path, name)
             print(f"a: {path}")
         for name in insc:
-            if name.startswith('.'):
-                continue
             path = osp.join(root_path, name)
-            check_(path, org[name], cur[name])
+            check_(path, org[name], cur[name], name_only)
     elif osp.islink(root_path) or osp.isfile(root_path):
-        if org != cur:
+        if not name_only and org != cur:
             print(f"c: {root_path}")
     else:
         assert (
@@ -76,24 +74,26 @@ def check_(root_path, org, cur):
 
 
 @click.command()
+@click.option("-n", "--name-only", is_flag=True, help="Ignoring the file content.")
 @click.argument("directory")
-def gen(directory):
+def gen(directory, name_only):
     """Generate folder content description file"""
-    file_tree = gen_(directory)
+    file_tree = gen_(directory, name_only)
     des_path = osp.join(directory, FDHASH_FILE)
     with open(des_path, "wt") as f:
         json.dump(file_tree, f, ensure_ascii=False, indent=2)
 
 
 @click.command()
+@click.option("-n", "--name-only", is_flag=True, help="Ignoring the file content.")
 @click.argument("directory")
-def check(directory):
+def check(directory, name_only):
     """Check against folder content description file"""
     des_path = osp.join(directory, FDHASH_FILE)
     with open(des_path, "rt") as f:
         org_tree = json.load(f)
-    cur_tree = gen_(directory)
-    check_(directory, org_tree, cur_tree)
+    cur_tree = gen_(directory, name_only)
+    check_(directory, org_tree, cur_tree, name_only)
 
 
 @click.group()
